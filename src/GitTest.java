0174cc17257f451b124df66afb40fecd105e3b92
techtorial <info@techtorialacademy.com>
2021-03-16 19:23:02 -0500

    First

---
+++ src/GitTest.java
@@ -0,0 +1,8 @@
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("100_000.00$");
+        System.out.println("10");
+        System.out.println("Muammer");
+    }
+}
