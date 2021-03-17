1669aa7a0ed48453f420defb665c6635374163eb
techtorial <info@techtorialacademy.com>
2021-03-16 19:31:10 -0500

    Scrum Runner Changes

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("100_000.00$");
         System.out.println("10");
         System.out.println("Muammer");
+        System.out.println("ScrumRunner Branch");
     }
 }
